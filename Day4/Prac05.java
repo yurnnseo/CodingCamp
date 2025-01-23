7dd247c4b3d4d5586df70a4623e089919a6a1826
yurnnseo <ysshinaja@naver.com>
2025-01-23 16:12:27 +0900

    Update Prac05.java

--- Day4/Prac05.java
+++ Day4/Prac05.java
@@ -18,10 +18,35 @@ public class Prac05 extends JFrame {
 		
 		private ImageIcon icon = new ImageIcon("due.png");
 		private Image img = icon.getImage();
+		private double width = img.getWidth(this);
+		private double height = img.getHeight(this);
+		
+		public MyPanel() {
+			setFocusable(true); // 키 입력을 받을 수 있도록 설정
+            requestFocus(); // 패널에 포커스 요청
+            
+			addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyPressed(KeyEvent e) {
+					if(e.getKeyChar() == '+') {
+						width *= 1.1;
+						height *= 1.1;
+						
+						repaint();
+					}
+					else if (e.getKeyChar() == '-'){
+						width *= 0.9;
+						height *= 0.9;
+						
+						repaint();
+					}
+				}
+			});
+		}
 		
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
-			g.drawImage(img, 10, 10, getWidth(), getHeight(), this);
+			g.drawImage(img, 10, 10, (int)width, (int)height, this);
 		}
 	}
 
